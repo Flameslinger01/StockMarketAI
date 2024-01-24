16a6e34602806da22fcab9095d4479355c18c6dc
Flameslinger01 <140361187+Flameslinger01@users.noreply.github.com>
2024-01-24 04:47:02 +0000

    Co-authored-by: VolnutVolnut <VolnutVolnut@users.noreply.github.com>

--- extract_data.py
+++ extract_data.py
@@ -9,7 +9,7 @@ class stock:
         self.volume = volume # list of ints, volume
         self.ticker = ticker # string, ticker name
 
-def generateStockList(data): # returns a list of a list containing stock class, shares, then buys
+def generateStockList(data, day, startingDay): # returns a list of a list containing stock class, shares, then buys
     data = np.genfromtxt(data, delimiter=',', dtype=None, encoding=None)
     stockList = []
     open = []
@@ -21,27 +21,31 @@ def generateStockList(data): # returns a list of a list containing stock class, 
     temp = stock(open,high,low,close,volume,ticker)
 
     for i in range(1, len(data)):
-        currentTicker = data[i,7]
-        if(currentTicker != ticker):
-            if(ticker != 'hola'):
-                temp = [stock(open,high,low,close,volume,ticker), 0, 0] # stock, shares, buy price
-                stockList.append(temp)
-            open = []
-            high = []
-            low = []
-            close = []
-            volume = []
-            ticker = currentTicker
+        currentDate = data[i,0]
+        if(currentDate[8:10] != str(startingDay + day)):
+            continue
         else:
-            open.append(data[i,1])
-            high.append(data[i,2])
-            low.append(data[i,3])
-            close.append(data[i,4])
-            volume.append(data[i,6])
+            currentTicker = data[i,7]
+            if(currentTicker != ticker):
+                if(ticker != 'hola'):
+                    temp = [stock(open,high,low,close,volume,ticker), 0, 0] # stock, shares, buy price
+                    stockList.append(temp)
+                open = []
+                high = []
+                low = []
+                close = []
+                volume = []
+                ticker = currentTicker
+            else:
+                open.append(data[i,1])
+                high.append(data[i,2])
+                low.append(data[i,3])
+                close.append(data[i,4])
+                volume.append(data[i,6])
         
     temp = [stock(open,high,low,close,volume,ticker), 0, 0]
     stockList.append(temp)
     return stockList
 
-test = generateStockList('data/minute.csv')
+test = generateStockList('data/minute.csv',0,17)
 print(test[0][0].ticker) # first company, first minute
--- market.py
+++ market.py
@@ -2,27 +2,57 @@
 import extract_data as ed
 import gymnasium as gym
 from gymnasium import spaces
+from gymnasium import utils
 import numpy as np
 
 
-class marketEnv(gym.Env):
-    def __init__(self, stockList):
-        """sedfsef"""
 
+class marketEnv(gym.Env):
+    # Initialization
+    def __init__(self, numberOfDays, numberOfStocks):
+        self.numberOfDays = numberOfDays
+        self.numberOfStocks = numberOfStocks
         # Observation Space
         self.observation_space = spaces.Dict(
             {
-                "botData": spaces.Box([25000 0], [3.4028235e+38 390], (2,), float32), # current money, current time
-                "investmentData": spaces.Box([0 0], [3.4028235e+38 3.4028235e+38], (2,4), float32), # shares, buy price
-                "currentStock": spaces.Box(0, 3.4028235e+38, (5,4), float32), # Open High Low Close Volume
+                "botData": spaces.Box(np.array([25000, 0]), np.array([3.4028235e+38, 390]), (2,), float), # current money, current time
+                "investmentData": spaces.Box(np.zeros([numberOfStocks,2], float), np.full_like(np.zeros([numberOfStocks,2]), [3.4028235e+38, 3.4028235e+38]), (numberOfStocks,2), float), # shares, buy price
+                "stocks": spaces.Box(0, 3.4028235e+38, (numberOfStocks,5), float), # Open High Low Close Volume
             }
         )
+
         # Action Space
-        self.action_space = spaces.Dict(4)
-        
-        self.bot_commands = spaces.Dict(
+        self.action_space = spaces.Dict(
             {
-                "buy": spaces.Box([],[],(), float32), # what stock, how many shares 
+                 "buy": spaces.Box([0, 1],[1001, 3.4028235e+38],(2,), float), # what stock, how many shares
+                 "sell": spaces.Box([0, 0],[1001, 3.4028235e+38],(2,), float), #what stock, how many shares
+                 "wait": np.ndarray([0,0])
             }
+
         )
-    
+
+    # Get observations from environment
+
+    def _get_obs(self):
+        """
+        test
+        """
+    # Reset 
+    def reset(self, seed=None, options=None):
+        # We need the following line to seed self.np_random
+        super().reset(seed=seed)
+
+        # Variables
+        self.fullMarket = ed.generateStockList('data/minute.csv', self.np_random.integers(0,1,1,int), 17)
+        self.currentMoney = self.np_random.integers(30000, 40000, 1, float)
+        self.currentTime = 0
+        self.investmentData = np.zeros([self.numberOfStocks,2], float)
+        
+        
+
+
+
+    def step(self, action):
+        stock_price = "investmentData" 
+        
+        
